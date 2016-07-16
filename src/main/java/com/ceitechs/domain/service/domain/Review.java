364049a2b5a95b63ac5215bb4bcdb76957826674
Iddy Magohe <iddyiam@gmail.com>
2016-07-16 19:46:01 -0400

    trimmed some spaces

--- src/main/java/com/ceitechs/domain/service/domain/Review.java
+++ src/main/java/com/ceitechs/domain/service/domain/Review.java
@@ -5,14 +5,20 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document(collection = "review")
+@TypeAlias("review")
 @Getter
 @Setter
-@ToString
 public class Review {
-    private User user;
-    private PropertyUnit propertyUnit;
-    private long reviewId;
+    @Id
+    private String reviewReferenceId;
+    private User tenant; // optional field, tenant being reviewed by landlord
+    private PropertyUnit propertyUnit; //// optional field, propertyUnit being reviewed by tenant
     private String reviewText;
     private double rating;
     private String reviewedBy;
