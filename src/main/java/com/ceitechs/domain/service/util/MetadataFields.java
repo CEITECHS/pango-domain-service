74a7cd53129426e10778ea6f2e792f405dfd2e42
Iddy Magohe <iddyiam@gmail.com>
2016-07-16 11:17:11 -0400

    Meta data updates for Pango attachments

--- src/main/java/com/ceitechs/domain/service/util/MetadataFields.java
+++ src/main/java/com/ceitechs/domain/service/util/MetadataFields.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.ceitechs.domain.service.util;
 
@@ -8,13 +8,14 @@ package com.ceitechs.domain.service.util;
  * @since 1.0
  */
 public interface MetadataFields {
-	String USER_REFERENCE_ID = "provider_id";
-	String PROPERTY_REFERENCE_ID="";
-	String UNIT_REFERENCE_ID="";
-	String TYPE = "type";
-	String CONTENT_TYPE = "contentType";
-	String THUMBNAIL = "thumbnail";
-	String FILE_NAME="filename";
-	String FILE_DESCR="description";
-	String REQURED_META="A minimum of filename and ContentType metadata is required";
+    String USER_REFERENCE_ID = "userReferenceId";
+    String PROPERTY_REFERENCE_ID = "propertyReferenceId";
+    String UNIT_REFERENCE_ID = "propertyUnitReferenceId";
+    String ENQUIRY_REFERENCE_ID = "enquiryReferenceId";
+    String TYPE = "type";
+    String CONTENT_TYPE = "contentType";
+    String THUMBNAIL = "thumbnail";
+    String FILE_NAME = "filename";
+    String FILE_DESCR = "description";
+    String REQURED_META = "A minimum of filename and ContentType metadata is required";
 }
