a607e94579c0d15f0dcd9be2c3b49a92b55eda54
Iddy Magohe <iddyiam@gmail.com>
2016-07-16 21:23:20 -0400

    Review test-cases

--- src/test/java/com/ceitechs/domain/service/repositories/PangoDomainServiceReviewRepositoryTest.java
+++ src/test/java/com/ceitechs/domain/service/repositories/PangoDomainServiceReviewRepositoryTest.java
@@ -74,5 +74,46 @@ public class PangoDomainServiceReviewRepositoryTest extends AbstractPangoDomainS
         reviewPage.forEach(System.out::println);
     }
 
+    @Test
+    public void saveReviewByLandlordTest() {
+        reviewRepository.deleteAll();
+        assertTrue(reviewRepository.findAll().size() == 0);
+        Review review = new Review();
+        review.setReviewReferenceId(PangoUtility.generateIdAsString());
+        User usr = userRepository.findAll().get(0);
+        assertNotNull(usr);
+        review.setReviewText("I liked this Tenant for his entire stay");
+        review.setTenantReferenceId(usr.getUserReferenceId());
+        review.setReviewedBy(String.format("%s, %s", usr.getFirstName(), usr.getLastName())); ////TODO set property Owner's names Reference
+        review.setRecommend(true);
+        review.setRating(4.6);
+        Review savedReview = reviewRepository.save(review);
+        assertNotNull(savedReview);
+    }
+
+    @Test
+    public void reviewsByTenantTest() {
+        reviewRepository.deleteAll();
+        User usr = userRepository.findAll().get(0);
+        assertNotNull(usr);
+        IntStream.range(0, 5).forEach(i -> {
+            Review review = new Review();
+            review.setReviewReferenceId(PangoUtility.generateIdAsString());
+            review.setTenantReferenceId(usr.getUserReferenceId());
+            review.setReviewText("I liked this Tenant for his entire stay Big times XXX - "+ i);
+            review.setReviewedBy(String.format("%s, %s", usr.getFirstName(), usr.getLastName())); ////TODO set property Owner's names Reference
+            review.setRecommend(true);
+            review.setRating(PangoUtility.random(4.0,5.0));
+            Review savedReview = reviewRepository.save(review);
+        });
+
+        Page<Review> reviewPage = reviewRepository.findByTenantReferenceIdOrderByCreatedDateDesc(usr.getUserReferenceId(), new PageRequest(0,50));
+        //assertN(reviewPage.iterator());
+        List<Review> reviews = PangoUtility.toArrayList(reviewPage.iterator());
+        assertFalse(reviews.isEmpty());
+        assertTrue(reviews.size() >= 4);
+        reviewPage.forEach(System.out::println);
+    }
+
 
 }
