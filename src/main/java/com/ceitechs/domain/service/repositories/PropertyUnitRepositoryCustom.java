52c0470e3a2b6fc62ba45cf2fb6917cbbf2a5dcf
Iddy Magohe <iddyiam@gmail.com>
2016-07-18 22:34:44 -0400

    impl of findAllPropertyUnits - pending test-cases

--- src/main/java/com/ceitechs/domain/service/repositories/PropertyUnitRepositoryCustom.java
+++ src/main/java/com/ceitechs/domain/service/repositories/PropertyUnitRepositoryCustom.java
@@ -4,9 +4,11 @@ package com.ceitechs.domain.service.repositories;
 import com.ceitechs.domain.service.domain.PropertySearchCriteria;
 import com.ceitechs.domain.service.domain.PropertyUnit;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.GeoResult;
-import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.*;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.NearQuery;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,6 +29,22 @@ class PropertyUnitRepositoryImpl implements PropertyUnitRepositoryCustom{
     @Override
     public GeoResults<PropertyUnit> findAllPropertyUnits(PropertySearchCriteria searchCriteria) {
 
-        return null;
+        Criteria criteria = Criteria.where("active").is(true);
+
+        criteria.and("purpose").is(PropertyUnit.PropertyPurpose.valueOf(searchCriteria.getPropertyPupose()));
+
+        criteria.and("nextAvailableDate").lt(searchCriteria.getMoveInDate().get());
+
+        criteria.and("features.nbrOfRooms").gte(searchCriteria.getRoomsCount());
+
+        criteria.orOperator(Criteria.where("features.nbrOfBedRooms").gte(searchCriteria.getBedRoomsCount()),
+                Criteria.where("features.nbrOfBaths").gte(searchCriteria.getBathCount()));
+
+        Point location = new Point(searchCriteria.getLatitude(), searchCriteria.getLongitude());
+
+        NearQuery near = NearQuery.near(location).maxDistance(new Distance(searchCriteria.getRadius(), Metrics.KILOMETERS));
+        near.query(new Query(criteria));
+
+        return mongoOperations.geoNear(near,PropertyUnit.class);
     }
 }
